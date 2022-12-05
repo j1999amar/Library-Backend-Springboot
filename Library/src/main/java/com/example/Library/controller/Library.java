23b3ffe5b455852f4d536ce9636a266e7a97cb6b
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-05 19:59:47 +0530

    BookList

--- Library/src/main/java/com/example/Library/controller/Library.java
+++ Library/src/main/java/com/example/Library/controller/Library.java
@@ -1,4 +1,12 @@
 package com.example.Library.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Library {
+    @PostMapping()
+    public String BookList(){
+        return "BookList";
+    }
 }
